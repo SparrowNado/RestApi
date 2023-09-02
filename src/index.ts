6f593cd5397f157f06ff3eb2498a6090f8f5f6c8
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-02 13:49:26 +0800

    updated modules

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import http from 'http';
  import cookieParser from  'cookie-parser';
  import compression from  'compression' ;
  import cors from 'cors'; 
+ import mongoose from 'mongoose';
 
 
  const app  = express();
@@ -26,3 +27,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('server running on http://localhost:8080');
 });
+
+const MONGO_URL = "mongodb+srv://newguy:<demo>@cluster0.hrbxhhg.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error' , (error: Error) => console.log(error)); 
